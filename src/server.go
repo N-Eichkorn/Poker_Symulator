3496770207b60ec0a2754a4484c08747be0bc6be
n-eichkorn <niklas.eichkorn@ametek.com>
2023-03-10 07:22:21 +0100

    sunshine

--- src/server.go
+++ src/server.go
@@ -2,4 +2,44 @@ package main
 
 func StartServer(port int) {
 
+	list := new(List)
+	rpc.Register(list)
+	tcpAddr, err := net.ResolveTCPAddr("tcp", ":1234")
+	checkError(err)
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+	/* This works:
+	   rpc.Accept(listener)
+	*/
+	/* and so does this:
+	 */
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		rpc.ServeConn(conn)
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("Fatal error ", err.Error())
+		os.Exit(1)
+	}
+}
+
+type Element struct {
+	Timestamp time.Time
+	Hostname  string
+	HW_Info   string
+}
+
+type List []Element
+
+func (l *List) Add(hostname string, reply *int) error {
+	*l = append(*l, Element{Timestamp: time.Now(), Hostname: hostname, HW_Info: ""})
+
+	fmt.Println(l)
+	return nil
 }
